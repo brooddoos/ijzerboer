cb7d5a06154c72895b65ada41fe2fa5d12bd9e9d
Kobe Geens <kobegeens@proton.me>
2025-11-16 00:23:52 +0100

    UNFINISHED CHANGES!
    -changed the resolution
    -added game scene
    -started working on car mechanics
    -improved naming consistency
    -added transition system from previous project
    -started changes to UI

---
+++ ijzerboer/scripts/menu.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+@onready var _3_dsplashtext: Label3D = $"../../logo/3dsplashtext"
+@onready var settings: PanelContainer = $Settings
+@onready var buttons: PanelContainer = $Buttons
+
+const splashTexts = [ #voeg later mss meer toe, idk
+	"im uhhh the iron metal collector man",
+	"Man, man, man, ... miserie, miserie",
+	"a great source of iron",
+	"a not so great source of iron",
+	"im picking up metals wow",
+]
+
+func _ready() -> void:
+	var rng = RandomNumberGenerator.new()
+	_3_dsplashtext.text = splashTexts[rng.randi_range(0,len(splashTexts)-1)]
+
+func _on_play_pressed() -> void:
+	Transition.changescene()
+
+func _on_options_pressed() -> void:
+	buttons.visible = !buttons.visible
+	settings.visible = !settings.visible
+	
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
