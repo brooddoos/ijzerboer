d77b2f8f3d9c4c7ee8263d78aaee3d25fd4c99ee
Kobe Geens <kobegeens@proton.me>
2025-12-29 16:42:31 +0100

    progress
    -added models
    -fixed bugs
    -broke camera??

--- ijzerboer/scripts/camera.gd
+++ ijzerboer/scripts/camera.gd
@@ -1,13 +1,13 @@
 extends Camera3D
 
-@export var camion:Node3D
+@export var car:Node3D
 @export var campos:Node3D
 
 func _physics_process(_delta : float):
 	var target_pos = lerp(global_position, campos.global_position, 0.1)
 
-	if target_pos.distance_to(camion.global_position) > 10.0:
-		target_pos = camion.global_position + (target_pos - camion.global_position).normalized() * 10.0
+	if target_pos.distance_to(car.global_position) > 10.0:
+		target_pos = car.global_position + (target_pos - car.global_position).normalized() * 10.0
 
 	global_position = target_pos
-	look_at(camion.global_position)
+	look_at(car.global_position)
